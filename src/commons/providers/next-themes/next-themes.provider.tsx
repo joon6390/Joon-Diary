ad5f7c0c026e28442db2e73a115b8bf6e0e49d6d
joon6390 <joon6390@naver.com>
2025-12-17 17:32:38 +0900

    feat: next-themes provider 구현 및 layout 연동
    - next-themes를 사용한 테마 설정 provider 구현
    - NextThemesProviderWrapper 컴포넌트 생성
    - layout.tsx에 NextThemesProvider 연동하여 children 감싸기
    - 기본 테마 설정: system, class attribute 사용

---
+++ src/commons/providers/next-themes/next-themes.provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
+
+interface NextThemesProviderProps {
+  children: ReactNode;
+}
+
+export const NextThemesProviderWrapper = ({
+  children,
+}: NextThemesProviderProps) => {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  );
+};
