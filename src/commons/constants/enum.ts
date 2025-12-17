92068ff591ed811965d1d6fa511fa442fc6a3300
joon6390 <joon6390@naver.com>
2025-12-17 17:00:33 +0900

    feat: 감정(emotion) enum 데이터 구현
    - Happy, Sad, Angry, Surprise, Etc 5가지 감정 타입 정의
    - 각 감정별 label, images(m/s), color 속성 구현
    - emotionDataMap으로 감정 데이터 관리
    - getEmotionData 유틸리티 함수 제공

---
+++ src/commons/constants/enum.ts
@@ -0,0 +1,93 @@
+/**
+ * Enum Constants
+ * 프로젝트에서 사용되는 enum 데이터 정의
+ */
+
+import { colors } from "./color";
+
+/**
+ * 감정(Emotion) Enum
+ */
+export enum EmotionType {
+  Happy = "HAPPY",
+  Sad = "SAD",
+  Angry = "ANGRY",
+  Surprise = "SURPRISE",
+  Etc = "ETC",
+}
+
+/**
+ * 감정 데이터 인터페이스
+ */
+export interface EmotionData {
+  type: EmotionType;
+  label: string;
+  images: {
+    medium: string;
+    small: string;
+  };
+  color: string;
+}
+
+/**
+ * 감정별 상세 데이터 맵
+ */
+export const emotionDataMap: Record<EmotionType, EmotionData> = {
+  [EmotionType.Happy]: {
+    type: EmotionType.Happy,
+    label: "행복해요",
+    images: {
+      medium: "emotion-happy-m.svg",
+      small: "emotion-happy-s.svg",
+    },
+    color: colors.red[60],
+  },
+  [EmotionType.Sad]: {
+    type: EmotionType.Sad,
+    label: "슬퍼요",
+    images: {
+      medium: "emotion-sad-m.svg",
+      small: "emotion-sad-s.svg",
+    },
+    color: colors.blue[60],
+  },
+  [EmotionType.Angry]: {
+    type: EmotionType.Angry,
+    label: "화나요",
+    images: {
+      medium: "emotion-angry-m.svg",
+      small: "emotion-angry-s.svg",
+    },
+    color: colors.gray[60],
+  },
+  [EmotionType.Surprise]: {
+    type: EmotionType.Surprise,
+    label: "놀랐어요",
+    images: {
+      medium: "emotion-surprise-m.svg",
+      small: "emotion-surprise-s.svg",
+    },
+    color: colors.yellow[60],
+  },
+  [EmotionType.Etc]: {
+    type: EmotionType.Etc,
+    label: "기타",
+    images: {
+      medium: "emotion-etc-m.svg",
+      small: "emotion-etc-s.svg",
+    },
+    color: colors.green[60],
+  },
+} as const;
+
+/**
+ * 모든 감정 데이터 배열
+ */
+export const emotionList: EmotionData[] = Object.values(emotionDataMap);
+
+/**
+ * 감정 타입으로 데이터 조회
+ */
+export const getEmotionData = (type: EmotionType): EmotionData => {
+  return emotionDataMap[type];
+};
