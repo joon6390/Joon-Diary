66a595c2ef7fd83ff270fb7218e892ee1cf80ed3
joon6390 <joon6390@naver.com>
2025-12-23 16:57:11 +0900

    feat(pictures): 와이어프레임 구조 구현

---
+++ src/components/pictures/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+export default function Pictures() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.gap1} />
+      <div className={styles.filter} />
+      <div className={styles.gap2} />
+      <div className={styles.main} />
+    </div>
+  );
+}
+
